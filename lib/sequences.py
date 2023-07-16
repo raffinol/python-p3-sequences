ba935b70ab1518a20adbeff0cf2185ab2a28947a
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-16 18:47:31 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+        if length > 1:
+            fibonacci_sequence.append(1)
+            if length > 2:
+                i = 2
+                while i < length:
+                    fibonacci_sequence.append(
+                        fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+                    )
+                    i += 1
+    print(fibonacci_sequence)
